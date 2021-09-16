e663ccc5f2952f5651a1998f3f439d094444eeef
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2021-09-16 18:18:58 -0500

    Update btStopCommand.php

--- src/IcyEndymion004/Commands/btStopCommand.php
+++ src/IcyEndymion004/Commands/btStopCommand.php
@@ -13,11 +13,34 @@ use IcyEndymion004\libs\jojoe77777\FormAPI\CustomForm;
 
 class btStopCommand{
 
+
+	public $bT = [];
+	public $btSetup = [];
+
 	public $plugin;
 
 	public function __construct(Loader $plugin){
 		$this->plugin = $plugin;
 	}
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+		$smcmd = strtolower($cmd);
+		$economy = $this->plugin->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance();
+		$prefix = $this->getConfig()->get("prefix");
+		switch($smcmd){
+			case "btstop":
+				if($this->plugin->isactive() !== false ){
+				$sender->sendMessage("There Is No Tournament Active");
+				return false;
+				}
+				$sender->sendMessage("§aYou Have Stoped The BLock Tournament");
+                foreach($this->plugin->getServer()->getOnlinePlayer() as $pl){
+                $pl->sendTitle("§a§lAn Admin Has Stopped The Block Tournament");
+				$pl->sendSubTitle("§cNo Rewards Have Been Given Sorry");
+				}
 
+				unset($this->plugin->btSetup);
+				unset($this->plugin->bT);
 
+		}
+	}
 }
