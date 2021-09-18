2ef2e2710392dfe5cebca35e69c39bd99005b061
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2021-09-18 18:54:48 -0500

    Update Loader.php

--- src/IcyEndymion004/Loader.php
+++ src/IcyEndymion004/Loader.php
@@ -4,6 +4,7 @@ namespace IcyEndymion004;
 
 use IcyEndymion004\Tournament\Tasks\TournamentHeartbeatTask;
 use pocketmine\plugin\PluginBase;
+use IcyEndymion004\libs\muqsit\invmenu\InvMenuHandler;
 
 class Loader extends PluginBase{
 
@@ -15,6 +16,25 @@ class Loader extends PluginBase{
     public function onEnable(): void{
         $this->saveDefaultConfig();
         $this->getScheduler()->scheduleRepeatingTask($this->heartbeatTask = new TournamentHeartbeatTask(), 20);
+        if (!$this->getConfig()->exists("config-version")) {
+			$this->getLogger()->notice("§eYour configuration file is from another version. Updating the Config...");
+			$this->getLogger()->notice("§eThe old configuration file can be found at config_old.yml");
+			rename($this->getDataFolder()."config.yml", $this->getDataFolder()."config_old.yml");
+			$this->saveResource("config.yml");
+			return;
+	  }
+	  if (version_compare("0.0.1", $this->getConfig()->get("config-version"))) {
+	  $this->getLogger()->notice("§eYour configuration file is from another version. Updating the Config...");
+			$this->getLogger()->notice("§eThe old configuration file can be found at config_old.yml");
+			rename($this->getDataFolder()."config.yml", $this->getDataFolder()."config_old.yml");
+			$this->saveResource("config.yml");
+			return;
+  }
+  if(!InvMenuHandler::isRegistered()){
+    InvMenuHandler::register($this);
+}
+$this->getServer()->getLogger()->info("Frozen Block Tournaments enabled!");
+
     }
 
     public function onLoad(): void{
