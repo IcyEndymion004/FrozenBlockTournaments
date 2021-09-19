74b10c0cb5b28ca31ec236cc540fa4494639e2ca
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2021-09-19 11:37:47 -0500

    Update btStartCommand.php

--- src/IcyEndymion004/Commands/btStartCommand.php
+++ src/IcyEndymion004/Commands/btStartCommand.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 namespace IcyEndymion004\Commands;
@@ -6,100 +5,71 @@ namespace IcyEndymion004\Commands;
 use pocketmine\command\Command;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
-use pocketmine\item\Item;
-use pocketmine\utils\TextFormat;
+use IcyEndymion004\Tournament\TournamentManager;
+use pocketmine\Player;
 use IcyEndymion004\Loader;
-use IcyEndymion004\libs\jojoe77777\FormAPI\Form;
+use pocketmine\Server;
 use IcyEndymion004\libs\jojoe77777\FormAPI\CustomForm;
+
 
 class btStartCommand implements CommandExecutor{
 
 	public $plugin;
+	public $TournyData;
+
 
 	public function __construct(Loader $plugin){
 		$this->plugin = $plugin;
 		$plugin->getCommand("btstart")->setExecutor($this);
 	}
 
-	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : void{
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 		$smcmd = strtolower($cmd);
-		$economy = $this->plugin->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance();
-		$prefix = $this->getConfig()->get("prefix");
+		$prefix = "§8[§1BT§8]§r";
 		switch($smcmd){
 			case "btstart":
-				if(!isset($args[1])){
-					$sender->sendMessage("$prefix Usage: §a/btstart <time> <prize type> <1st Place Prize> <2nd Place Prize> <3rd Place Prize> <4th Place Prize> <5th Place Prize>", false);
+				if(!isset($args[0])){
+					$this->plugin->getCommand("btstart")->setUsage("$prefix §cUsage: §a/btstart <prize type> <1st Place Prize> <2nd Place Prize> <3rd Place Prize> <4th Place Prize> <5th Place Prize>", false);
 					return true;
-				}
-						if(!is_numeric($args[0])){
-							$sender->sendMessage("$prefix §c§oTournament time must be numeric!");
-							return true;
-						}
-						if($args[0] > 120){
-							$sender->sendMessage("$prefix §c§oTournaments can only be 120 minutes long!");
-							return true;
-						}
-						if($args[0] < 1){
-							$sender->sendMessage("$prefix §c§oTournaments must be at least 1 minutes long!");
-							return true;
-						}
-						$time = ($args[0] * 60);
-
-						$prizetype = $args[1];
-
-                                $TopPlacementReward = (int) $args[2];
-								if(!is_int($TopPlacementReward)){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be between $1-$10,000,000!");
-								}elseif($TopPlacementReward < 1){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be more than $1!");
-								}elseif($TopPlacementReward > 10000000){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be less than $10,000,000!");
-								}	
-								$SecondPlacementReward = (int) $args[3];
-								if(!is_int($SecondPlacementReward)){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be between $1-$10,000,000!");
-								}elseif($SecondPlacementReward < 1){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be more than $1!");
-								}elseif($SecondPlacementReward > 10000000){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be less than $10,000,000!");
+				};	
+                                $prizetype = $args[0];
+								Loader::getInstance()->getTournydata()->set("PrizeType", $prizetype);
+								Loader::getInstance()->getTournydata()->save();
+								if($args[0] == "cash"){
+                                    $form = new CustomForm(function(Player $player, $data){
+									$TopPlacementReward = $data[1];
+                                    $SecondPlacementReward = $data[2];
+                                    $ThridPlacementReward = $data[3];
+                                    $ForthPlacementReward = $data[4];
+                                    $FifthtPlacementReward = $data[5];
+									Loader::getInstance()->getTournydata()->set("TopPlacementReward", $TopPlacementReward);
+									Loader::getInstance()->getTournydata()->set("SecondPlacementReward", $SecondPlacementReward);
+									Loader::getInstance()->getTournydata()->set("ThridPlacementReward", $ThridPlacementReward);
+									Loader::getInstance()->getTournydata()->set("ForthPlacementReward", $ForthPlacementReward);
+									Loader::getInstance()->getTournydata()->set("FifthtPlacementReward", $FifthtPlacementReward);
+									Loader::getInstance()->getTournydata()->save();							
+									});
+									$form->setTitle("§aPrizeType§8: §6Cash");
+									$form->addLabel("§eCash Reward");
+									$form->addInput("§61stPlaceReward", "§dMake Sure this Is a number");
+									$form->addInput("§72ndPlaceReward", "§dMake Sure this Is a number");
+									$form->addInput("§c3rdPlaceReward", "§dMake Sure this Is a number");
+									$form->addInput("§e4thPlaceReward", "§dMake Sure this Is a number");
+									$form->addInput("§95thPlaceReward", "§dMake Sure this Is a number");
+									$sender->sendForm($form);
+							    } 
 								}
-								$ThridPlacementReward = (int) $args[4];
-								if(!is_int($ThridPlacementReward)){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be between $1-$10,000,000!");
-								}elseif($ThridPlacementReward < 1){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be less than $10,000,000!");
+								foreach(Server::getInstance()->getOnlinePlayers() as $pl){
+								$this->TournyTitle($pl);
 								}
-								$ForthPlacementReward = (int) $args[5];
-								if(!is_int($ForthPlacementReward)){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be between $1-$10,000,000!");
-								}elseif($ForthPlacementReward < 1){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be more than $1!");
-								}elseif($ForthPlacementReward > 10000000){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be less than $10,000,000!");
-								}
-						        $FifthtPlacementReward = (int) $args[6];
-								if(!is_int($FifthtPlacementReward)){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be between $1-$10,000,000!");
-								}elseif($FifthtPlacementReward < 1){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be more than $1!");
-								}elseif($FifthtPlacementReward > 10000000){
-									$sender->sendMessage("$prefix §c§oInvalid Cash amount! Cash must be be less than $10,000,000!");
-								}
-								$this->plugin->btSetup["time"] = $time;
-								$this->plugin->btSetup["timestamp"] = time();
-								$this->plugin->btSetup["prizetype"] = $prizetype;
-								$this->plugin->btSetup["1stPlaceReward"] = $TopPlacementReward;
-								$this->plugin->btSetup["2ndPlaceReward"] = $SecondPlacementReward;
-								$this->plugin->btSetup["3rdPlaceReward"] = $ThridPlacementReward;
-								$this->plugin->btSetup["4thPlaceReward"] = $ForthPlacementReward;
-								$this->plugin->btSetup["5thPlaceReward"] = $FifthtPlacementReward;
-								foreach($this->plugin->getServer()->getOnlinePlayer() as $pl){
-									$pl->sendTitle("§1Block §4mining §dTournament §0Is §2Active");
-									$pl->sendSubTitle("§6Mine as many blocks as you can to win!");
-									$pl->sendMessage("§aTo See The Prize Amounts For The Top Prizes Do §6/btprizes");
-								}
-								}
+								return true;
+							}
+							public function TournyTitle(): void{
+									TournamentManager::setOpen(true);
+									foreach(Server::getInstance()->getOnlinePlayers() as $pl){
+										$pl->sendTitle("§1Block §4mining §dTournament §0Is §2Active");
+										$pl->sendSubTitle("§6Mine as many blocks as you can to win!");
+										$pl->sendMessage("§aTo See The Prize Amounts For The Top Prizes Do §6/btprizes");
+									}
 							}
 				        }
-=======
->>>>>>> Stashed changes
