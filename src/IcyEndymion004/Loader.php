83f1d6ed01744e2a0726163b522786d929eaa021
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2021-09-19 11:38:12 -0500

    Update Loader.php

--- src/IcyEndymion004/Loader.php
+++ src/IcyEndymion004/Loader.php
@@ -4,7 +4,14 @@ namespace IcyEndymion004;
 
 use IcyEndymion004\Tournament\Tasks\TournamentHeartbeatTask;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 use IcyEndymion004\libs\muqsit\invmenu\InvMenuHandler;
+use IcyEndymion004\Commands\btStartCommand;
+use IcyEndymion004\Commands\btStopCommand;
+use IcyEndymion004\Commands\btTopCommand;
+use IcyEndymion004\Commands\btPrizesCommand;
+use IcyEndymion004\Commands\btBrokenCommand;
+
 
 class Loader extends PluginBase{
 
@@ -14,7 +21,15 @@ class Loader extends PluginBase{
     public $heartbeatTask;
 
     public function onEnable(): void{
-        $this->saveDefaultConfig();
+
+        $prefix = $this->getConfig()->get("prefix");
+        $this->TournyData = new Config($this->getDataFolder() . "TournyData.yml", Config::YAML);
+        $this->cmds = new btStartCommand($this);
+		//$this->cmds = new btStopCommand($this);
+		//$this->cmds = new btPrizesCommand($this);
+		//$this->cmds = new btTopCommand($this);
+		//$this->cmds = new btBrokenCommand($this);
+        $this->saveDefaultConfig(); 
         $this->getScheduler()->scheduleRepeatingTask($this->heartbeatTask = new TournamentHeartbeatTask(), 20);
         if (!$this->getConfig()->exists("config-version")) {
 			$this->getLogger()->notice("§eYour configuration file is from another version. Updating the Config...");
@@ -40,7 +55,9 @@ $this->getServer()->getLogger()->info("Frozen Block Tournaments enabled!");
     public function onLoad(): void{
         self::$instance = $this;
     }
-
+    public function getTournydata(): Config{
+        return $this->TournyData;
+    }
     public static function getInstance(): Loader{
         return self::$instance;
     }
